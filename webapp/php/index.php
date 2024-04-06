7ba0f11813d87f5670abadcf2e5e6e81530990c2
kyoya0819 <kyoya0819@gmail.com>
2024-04-06 11:00:23 +0900

    Calculate Passhash in PHP

--- webapp/php/index.php
+++ webapp/php/index.php
@@ -192,19 +192,9 @@ function validate_user($account_name, $password) {
     return true;
 }
 
-function digest($src) {
-    // opensslのバージョンによっては (stdin)= というのがつくので取る
-    $src = escapeshellarg($src);
-    return trim(`printf "%s" {$src} | openssl dgst -sha512 | sed 's/^.*= //'`);
-}
-
-function calculate_salt($account_name) {
-    return digest($account_name);
-}
-
 function calculate_passhash($account_name, $password) {
-    $salt = calculate_salt($account_name);
-    return digest("{$password}:{$salt}");
+    $salt = hash("sha512", $account_name);
+    return hash("sha512", "{$password}:{$salt}");
 }
 
 // --------
